0e35a3f7460d61bfb42513696258d75e745825f5
Jenny Fang <jennyfang6@gmail.com>
2011-11-09 16:42:36 -0800

    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("Hello, Jenny!");
+		System.out.println("Jenny: Hiiiii");
 	}
     
     public void test(){
