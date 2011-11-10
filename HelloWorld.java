614b84c4c42ed5de6b2b328a113cad19037dcdf8
Mitch Tee <mitch.tee@gmail.com>
2011-11-09 16:17:30 -0800

    hello jenny

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello, Mitch!");
+		System.out.println("Hello, Jenny!");
 	}
     
     public void test(){
