f46b4c1e795bb36d28b32f4c3c9267cada37c984
Mitch Tee <mitch.tee@gmail.com>
2011-11-14 22:08:54 -0800

    added constructors, more comments.

--- tables/Borrowing.java
+++ tables/Borrowing.java
@@ -3,9 +3,17 @@ package tables;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Collection;
+import java.util.GregorianCalendar;
+import users.Conn;
 
+/**
+ * This class represents a Borrowing for a BookCopy by a Borrower.
+ * @author Mitch
+ */
 public class Borrowing implements Table {
 
 	private Integer borid;
@@ -16,47 +24,126 @@ public class Borrowing implements Table {
 	
 	Connection con;
 	Statement stmt;
+        
+        /**
+         * Creates an empty Borrowing
+         */
+        public Borrowing()
+        {
+          con = Conn.getInstance().getConnection();
+        }
+        
+        /**
+         * Creates a Borrowing with user specified fields
+         * @param borid the unique borrowing ID of this Borrowing
+         * @param outDate the date that BookCopy was loaned out; the date that
+         *  this Borrowing was created
+         * @param inDate the date that the BookCopy was returned to the library
+         * @param bc the book copy that this Borrowing is for
+         * @param borr the Borrower who borrowed this Borrowing
+         * @throws IllegalArgumentException if the future comes before the past
+         */
+        public Borrowing( Integer borid, 
+                          Calendar outDate, 
+                          Calendar inDate, 
+                          BookCopy bc,
+                          Borrower borr)
+        {
+          con = Conn.getInstance().getConnection();
+          this.borid = borid;
+          this.outDate = outDate;
+          verifyDateOrder(outDate, inDate);
+          this.inDate = inDate;
+          this.bc = bc;
+          this.borr = borr;
+        }
 	
+        /**
+         * Calculates the due date of the Borrowing.
+         * @pre borr is initialized
+         * @pre outDate is initialized
+         * @return the date that the Borrowing is due back to the library
+         * @throws NullPointerException if outDate or borr is not initialized
+         */
 	public Calendar getDueDate() {
 		return null;
 	}
 	
-	
+	/**
+         * Returns a matrix representation of the Borrowing table with
+         * every field shown as a String.
+         * @return a 2D array of strings where each row represents a tuple
+         * of the Borrowing table and every column represents a field
+         */
 	@Override
 	public String[][] display() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+        /**
+         * Updates the tuple in the Borrowing table whose primary key corresponds to
+         * this Borrowing's borid.  Please make sure there are no null fields
+         * unless you want the field to appear as null in the database.
+         */
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
 		
 	}
 
+        /**
+         * Deletes the tuple in the Borrowing table whose primary key
+         * corresponds to this Borrowing's borid.  All other attributes are
+         * ignored.
+         * @return true if the tuple was successfully deleted, otherwise false
+         */
 	@Override
 	public boolean delete() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+        /**
+         * Gets every Borrowing from the database and makes them into 
+         * Borrowing objects.  Stores them in a collection and returns them.
+         * If there are no Borrowings in the database the collection will be
+         * empty.
+         * @return a collection containing all the Borrowing objects in the
+         * database
+         */
 	@Override
 	public Collection<Table> getAll() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+        /**
+         * Gets the tuple from the Borrowing table that corresponds to this
+         * Borrowings's borid and returns it.  No attributes in the Borrowing
+         * object that calls this method are modified.
+         * @return a new Borrowing object whose attributes are set based on
+         * the tuple in the database that shares the calling object's borid
+         */
 	@Override
 	public Table get() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+        /**
+         * Creates a new entry in the database for this Borrowing object.
+         * A new borid will be generated, and the calling object's borid
+         * attribute will be mutated to the new borid.
+         * @return true if the Borrowing was successfully inserted
+         * otherwise false
+         * @post this object's borid will be the newest borid in the database
+         */
 	@Override
 	public boolean insert() {
 		try {
-			stmt.executeUpdate("INSERT INTO Borrowing VALUES (" + borid + ", " + outDate + ", " +
-			inDate + ", " + bc + ", " + borr + ")");
+			stmt.executeUpdate("INSERT INTO Borrowing VALUES (" + getBorid() + ", " + getOutDate() + ", " +
+			getInDate() + ", " + getBc() + ", " + getBorr() + ")");
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
@@ -65,4 +152,114 @@ public class Borrowing implements Table {
 		return true;
 	}
 
+  /**
+   * @return the borid
+   */
+  public Integer getBorid() {
+    return borid;
+  }
+
+  /**
+   * @param borid the borid to set
+   */
+  public void setBorid(Integer borid) {
+    this.borid = borid;
+  }
+
+  /**
+   * @return the outDate
+   */
+  public Calendar getOutDate() {
+    return outDate;
+  }
+
+  /**
+   * @param outDate the outDate to set
+   */
+  public void setOutDate(Calendar outDate) {
+    this.outDate = outDate;
+  }
+
+  /**
+   * @return the inDate
+   */
+  public Calendar getInDate() {
+    return inDate;
+  }
+
+  /**
+   * @param inDate the inDate to set
+   * @pre inDate must be after outDate
+   * @throws IllegalArgumentException if the inDate comes before the outDate
+   */
+  public void setInDate(Calendar inDate) {
+    verifyDateOrder(outDate, inDate);
+    this.inDate = inDate;
+  }
+
+  /**
+   * @return the bc
+   */
+  public BookCopy getBc() {
+    return bc;
+  }
+
+  /**
+   * @param bc the bc to set
+   */
+  public void setBc(BookCopy bc) {
+    this.bc = bc;
+  }
+
+  /**
+   * @return the borr
+   */
+  public Borrower getBorr() {
+    return borr;
+  }
+
+  /**
+   * @param borr the borr to set
+   */
+  public void setBorr(Borrower borr) {
+    this.borr = borr;
+  }
+
+  /**
+   * Throws an exception if the future date comes before the past date
+   * @param past
+   * @param future 
+   * @throws IllegalArgumentException if the future comes before the past
+   */
+  private static void verifyDateOrder(Calendar past, Calendar future)
+  {
+    if (future == null)
+    {
+      return;
+    }
+    boolean futureWithoutAPast = past == null/* && future != null*/;
+    if (futureWithoutAPast || future.before(past))
+    {
+      DateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+      throw new IllegalArgumentException("Time paradox! Cannot return borrowing"
+              + "before it is borrowed. (out date: "
+              +((past == null) ? "null" : df.format(past.getTime()) )
+              +"; in date: "+df.format(future.getTime())+")");
+    }
+  
+  }
+  
+  public static void main(String[] args) {
+    Calendar past = new GregorianCalendar(1983,11,27);
+    Calendar future = new GregorianCalendar(2004, 12, 14);
+    verifyDateOrder(past, future);
+    try
+    {
+      verifyDateOrder(future, past);
+    }
+    catch (Exception e)
+    {
+      System.out.println("successfully caught exception");
+    }
+  }
 }
--- tables/Fine.java
+++ tables/Fine.java
@@ -1,7 +1,11 @@
 package tables;
 
+import java.sql.Connection;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Collection;
+import users.Conn;
 
 public class Fine implements Table {
 
@@ -10,26 +14,85 @@ public class Fine implements Table {
 	private Calendar issueDate;
 	private Calendar paidDate;
 	private Borrowing b;
+        private Connection con;
+        
+        /**
+         * Creates an empty Fine object
+         */
+        public Fine()
+        {
+          con = Conn.getInstance().getConnection();
+        }
+        
+        /**
+         * Creates a new Fine object
+         * @param fid
+         * @param amount
+         * @param issueDate
+         * @param paidDate
+         * @param borrowing 
+         * @throws IllegalArgumentException if the paidDate comes before
+         *  the issueDate
+         */
+        public Fine(  Integer fid,
+                      Integer amount,
+                      Calendar issueDate,
+                      Calendar paidDate,
+                      Borrowing borrowing)
+        {
+          this.fid = fid;
+          this.amount = amount;
+          this.issueDate = issueDate;
+          verifyDateOrder(issueDate, paidDate);
+          this.paidDate = paidDate;
+          this.b = borrowing;
+        }
 	
-	
+	/**
+         * Generates a matrix of strings that represents the database's
+         * table structure for the Fine table.
+         * Each row represents a tuple in the Fine table and each column
+         * represents a field in the tuple table.
+         * @return a matrix of strings representing the structure of the Fine
+         *  table
+         */
 	@Override
 	public String[][] display() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+        /**
+         * Updates the Fine tuple whose fid matches the fid of this Fine object.
+         * All fields will be updated so please do not leave any attributes
+         * of this object null unless you want nulls to appear in the database.
+         * @pre the fid of this object must match an fid already in the database
+         * @post the Fine table in the database is updated
+         */
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
 		
 	}
 
+        /**
+         * Deletes the Fine tuple whose fid matches the fid of this Fine object.
+         * @return true if the delete was successful otherwise false
+         * @pre fid must already exist in the database
+         */
 	@Override
 	public boolean delete() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+        /**
+         * Creates a new entry in the Fine table, where each column matches
+         * the corresponding attribute of this Fine object.  The fid will be
+         * generated in the database, and the fid of this object will be mutated
+         * to hold the new fid.
+         * @return true if the entry was successfully created otherwise false.
+         */
 	@Override
 	public boolean insert() {
 		// TODO Auto-generated method stub
@@ -37,15 +100,126 @@ public class Fine implements Table {
 	}
 
 	@Override
+        /**
+         * Gets every entry of the Fine table and creates a Fine object for it.
+         * All objects are stored in a collection and returned.
+         * @return a collection of all Fines.
+         */
 	public Collection<Table> getAll() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+        /**
+         * Gets the Fine tuple whose fid matches this object's fid and creates
+         * a new Fine object that represents the tuple.
+         * @return a Fine object representing the Fine tuple in the database
+         *  with this object's fid.
+         */
 	@Override
 	public Table get() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+  /**
+   * @return the fid
+   */
+  public Integer getFid() {
+    return fid;
+  }
+
+  /**
+   * @param fid the fid to set
+   */
+  public void setFid(Integer fid) {
+    this.fid = fid;
+  }
+
+  /**
+   * The amount is in cents.
+   * @return the amount
+   */
+  public Integer getAmount() {
+    return amount;
+  }
+
+  /**
+   * The amount is in cents.
+   * @param amount the amount to set
+   */
+  public void setAmount(Integer amount) {
+    this.amount = amount;
+  }
+
+  /**
+   * @return the issueDate
+   */
+  public Calendar getIssueDate() {
+    return issueDate;
+  }
+
+  /**
+   * @param issueDate the issueDate to set
+   */
+  public void setIssueDate(Calendar issueDate) {
+    this.issueDate = issueDate;
+  }
+
+  /**
+   * @return the paidDate
+   */
+  public Calendar getPaidDate() {
+    return paidDate;
+  }
+
+  /**
+   * @param paidDate the paidDate to set
+   * @pre the paidDate must come after the issueDate 
+   * @throws IllegalArgumentException if the paidDate comes before
+   *  the issueDate
+   */
+  public void setPaidDate(Calendar paidDate) {
+    verifyDateOrder(issueDate, paidDate);
+    this.paidDate = paidDate;
+  }
+
+  /**
+   * @return the borrowing
+   */
+  public Borrowing getBorrowing() {
+    return b;
+  }
+
+  /**
+   * @param b the borrowing to set
+   */
+  public void setBorrowing(Borrowing b) {
+    this.b = b;
+  }
+  
+  /**
+   * Throws an exception if the future date comes before the past date
+   * @param past
+   * @param future 
+   * @throws IllegalArgumentException if the future comes before the past
+   */
+  private static void verifyDateOrder(Calendar past, Calendar future)
+  {
+    if (future == null)
+    {
+      return;
+    }
+    boolean futureWithoutAPast = past == null/* && future != null*/;
+    if (futureWithoutAPast || future.before(past))
+    {
+      DateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+      throw new IllegalArgumentException("Time paradox! Fine's paid date cannot come"
+              + " before the fine's issue date. Issue date: "
+              +((past == null) ? "null" : df.format(past.getTime()) )
+              +"; paid date: "+df.format(future.getTime())+")");
+    }
+  
+  }
 
 }
