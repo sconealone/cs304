881e5eb66eacc65bac34626205d9dac2fc7157e4
Mitch Tee <mitch.tee@gmail.com>
2011-11-14 17:16:38 -0800

    changed ndate to update (sorry for my messy writing); changed the return type for getAll to a collection of tables.

--- tables/Table.java
+++ tables/Table.java
@@ -1,16 +1,18 @@
 package tables;
 
+import java.util.Collection;
+
 public interface Table {
 	
 	public void display(String[][] arg);
 	
-	public void ndate();
+	public void update();
 	
 	public boolean delete();
 	
 	public boolean insert();
 	
-	public Table getAll();
+	public Collection<Table> getAll();
 	
 	public Table get();
 	
