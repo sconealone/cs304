0230169e40d6038062f5ef07f1a0410dca27ffb6
Mitch Tee <mitch.tee@gmail.com>
2011-11-26 17:05:09 -0800

    junit test hangs on insert. tests done in main pass

--- test/BookCopyTest.java
+++ test/BookCopyTest.java
@@ -1,109 +1,135 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package test;
 
+import tables.*;
+import java.sql.ResultSetMetaData;
+import java.util.Collection;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
+/**
+ * insert_test_values_medium.sql
+ * @author Mitch
+ */
 public class BookCopyTest {
+  
+  public BookCopyTest() {
+  }
 
-	@Before
-	public void setUp() throws Exception {
-	}
+  @BeforeClass
+  public static void setUpClass() throws Exception {
+  }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+  @AfterClass
+  public static void tearDownClass() throws Exception {
+  }
+  
+  @Before
+  public void setUp() {
+  }
+  
+  @After
+  public void tearDown() {
+  }
 
-	@Test
-	public final void testBookCopy() {
-		fail("Not yet implemented"); // TODO
-	}
+  /**
+   * Test of update method, of class BookCopy.
+   */
+  @Test
+  public void testUpdate() throws Exception {
+    System.out.println("update");
+    BookCopy bcupdate = new BookCopy();
+    Book bcupdatebook = new Book();
+    bcupdatebook.setCallNumber("AK315 X383 1999");
+    bcupdate.setB(bcupdatebook);
+    bcupdate.setCopyNo("C2");
+    bcupdate.setStatus("out");
+    bcupdate.update();
+    
+    bcupdate.setStatus(null);
+    bcupdate = (BookCopy) bcupdate.get();
+    assertEquals(bcupdate.getStatus(), "out");
+  }
 
-	@Test
-	public final void testBookCopyStringBookString() {
-		fail("Not yet implemented"); // TODO
-	}
 
-	@Test
-	public final void testDisplay() {
-		fail("Not yet implemented"); // TODO
-	}
+  /**
+   * Test of insert method, of class BookCopy.
+   */
+  @Test
+  public void testInsert() throws Exception {
+    System.out.println("insert");
+    BookCopy bcinsert = new BookCopy();
+    Book bcinsertbook = new Book();
+    bcinsertbook.setCallNumber("WY273 P213 1986");
+    bcinsert.setB(bcinsertbook);
+    bcinsert.insert();
+    bcinsert.setCopyNo("C"+200);
+    bcinsert.insert();
+    bcinsert.setCopyNo(null);
+    bcinsert.insert();
+    
+    bcinsert.setCopyNo("C200");
+    BookCopy c100 = (BookCopy)bcinsert.get();
+    assertEquals(c100.getB().getCallNumber(), "WY273 P213 1986");
+    bcinsert.setCopyNo("C201");
+    BookCopy c101 = (BookCopy) bcinsert.get();
+    System.out.println(c101.getB());
+    System.out.println(c101.getB().getCallNumber());
+    assertEquals(c101.getB().getCallNumber(), "WY273 P213 1986");
+  }
+  
+  /**
+   * Test of delete method, of class BookCopy.
+   */
+  @Test
+  public void testDelete() throws Exception {
+    System.out.println("delete");
+    
+    BookCopy bcdel = new BookCopy();
+    Book bcdelbook = new Book();
+    bcdelbook.setCallNumber("WY273 P213 1986");
+    bcdel.setB(bcdelbook);
+    bcdel.setCopyNo("C200");
+    assertEquals(bcdel.delete(), true);
+    bcdel.setCopyNo("C201");
+    
+    
+    assertEquals(bcdel.delete(), true);
+    assertEquals(bcdel.delete(),false);
+  }
 
-	@Test
-	public final void testUpdate() {
-		fail("Not yet implemented"); // TODO
-	}
 
-	@Test
-	public final void testDelete() {
-		fail("Not yet implemented"); // TODO
-	}
 
-	@Test
-	public final void testInsert() {
-		fail("Not yet implemented"); // TODO
-	}
 
-	@Test
-	public final void testGetAll() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetMeta() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetAllBook() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGet() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetStringBook() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetCopyNo() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testSetCopyNo() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetStatus() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testSetStatus() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetB() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testSetB() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetLatestCopyNo() {
-		fail("Not yet implemented"); // TODO
-	}
+  /**
+   * Test of get method, of class BookCopy.
+   */
+  @Test
+  public void testGet_0args() throws Exception {
+    System.out.println("get");
+    
+    BookCopy bcget = new BookCopy();
+    Book bcgetbook = new Book();
+    bcgetbook.setCallNumber("GV345 R202 1997");
+    bcget.setB(bcgetbook);
+    bcget.setCopyNo("C1");
+    bcget = (BookCopy) bcget.get();
+    
+    assertEquals("GV345 R202 1997",bcget.getB().getCallNumber());
+    assertEquals("C1", bcget.getCopyNo());
+    assertEquals("in", bcget.getStatus());
+  }
+  
+  public static void main(String[] args) throws Exception {
+    (new BookCopyTest()).testInsert();
+    (new BookCopyTest()).testDelete();
+  }
 
 }
